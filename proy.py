3262400f0827dd1c07bc7ff5a57c4fc4be2f40bc
Alejandro Huerta <alejandro.huerta80@uabc.edu.mx>
2025-11-23 17:26:16 -0800

    segundo commit

--- proy.py
+++ proy.py
@@ -44,11 +44,11 @@ class HashTable:
         print("Producto agregado correctamente.\n")
     
     def get(self, key):
-        h = self._hash(key) #computer hash for the given key
+        h = self._hash(key)
         while self.slots[h] is not None:
-            if self.slots[h].key is key:
-                return self.slots[h].value
-            h = (h+ 1) % self.size
+            if self.slots[h] != self._deleted and self.slots[h].key == key:
+                return self.slots[h]
+            h = (h + 1) % self.size
         return None
     def show_all(self):
         for slot in self.slots:
@@ -78,11 +78,12 @@ class Node(object):
         self.next = next
         self.prev = prev
 
-class cola:
-    def __init__(self):
+class queue:
+    def __init__(self, inventario):
         self.head = None
         self.tail = None
         self.count = 0
+        self.inventario = inventario
         
     def enqueue(self, data):
         new_node = Node(data, None, None)
@@ -105,30 +106,79 @@ class cola:
             self.head.prev = None
             self.count -=1 
         return current
-    
+    def show_queue(self):
+        if self.head is None:
+            print("La cola está vacía.")
+            return
+
+        print("Contenido de la cola:")
+        actual = self.head
+        while actual is not None:
+            print(actual.data)   # mostrar el dato del nodo
+            actual = actual.next
+            
+    def ant_dequeue(self):
+        nombre = input("nombre: ")
+        producto = input('producto: ')
+        
+        item = self.inventario.get(producto)
+        
+        if item is None:
+            print("el producto no existe en el inventario")
+            return
+        if int(item.cant) <= 0:
+            print('no hay stock disponible')
+            return
+        
+        newpedido = pedido(nombre, producto)
+        self.enqueue(newpedido)
+        
+        item.cant = str(int(item.cant) - 1)
+        
+        
+        
 class pedido:
     _contador_id = 1
     
     def __init__(self, solicitante, producto):
-        self.id += 1
+        self.id = pedido._contador_id
+        pedido._contador_id += 1
+
         self.solicitante = solicitante
         self.producto = producto
         
-
-
+        
+    def __str__(self):
+        return f"Pedido #{self.id} - {self.solicitante} pidió {self.producto}"
+    
+        
 
         
 def inventario():
     inv.show_all()
-    resp = int(input('que deseas hacer? \n 1. Insertar \n 2. Eliminar'))
+    resp = int(input('que deseas hacer? \n 1. Insertar \n 2. Eliminar \n 3. regresar \n'))
     if resp == 1:
         inv.put_interactivo()
     elif resp == 2:
         inv.delete_inter()
-    
+    else:
+        return
+        
+        
+def colaa():
+    cola.show_queue()
+    resp = int(input('que deseas hacer? \n 1. insertar? \n 2. tomar el pedido \n 3. regresar \n' ))
+    if resp == 1:
+        cola.ant_dequeue()
+    elif resp == 2:
+        nodo = cola.dequeue()
+        print(f'pedido tomado y eliminado de la cola: \n {nodo.data}\n')
+    else:
+        return
     
 
 inv = HashTable()
+cola = queue(inv)
 
 while True:
     
@@ -137,14 +187,14 @@ while True:
     print('que desea hacer?')
     print('1. ver inventario:')
     print('2. ver cola')
+    print('3. salir')
     resp = int(input())
     
     if resp == 1:
         inventario()
-    
-    
-    ss = input('desea salir?')
-    if ss == 's':
+    elif resp == 2:
+        colaa()
+    elif resp == 3:
         break
 
 
